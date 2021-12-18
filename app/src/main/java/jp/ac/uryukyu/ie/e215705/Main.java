5a75cbb8f141502e711a40000f09f89c5a9e59e2
Kouya Arakaki <e215705@ie.u-ryukyu.ac.jp>
2021-12-18 10:58:34 +0900

    Step3 commit

--- app/src/main/java/jp/ac/uryukyu/ie/e215705/Main.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215705/Main.java
@@ -2,13 +2,7 @@ package jp.ac.uryukyu.ie.e215705;
 
 public class Main {
     public static void main(String[] args) {
-        String str = null;
-        try {
-            System.out.println(str.length());
-        } catch (NullPointerException e) {
-            System.out.println("エラー!　NullPointerException　が発生！！");
-            System.out.println(e.getMessage());
-        }
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
     }
-
 }
